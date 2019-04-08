37e7829d0de984294469a2471faf9a7391d4e96a
Djuro Alfirevic <djalfirevic@gmail.com>
2019-04-08 19:24:07 +0200

    HelloVapor project added

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,10 +11,32 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("hello", "vapor") { req in
+        return "Hello, Vapor!"
+    }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        
+        return "Hello \(name)!"
+    }
+    
+    router.post(InfoData.self, at: "info") { req, data -> InfoResponse in
+        return InfoResponse(request: data)
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
